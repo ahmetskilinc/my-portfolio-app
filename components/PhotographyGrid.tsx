6e45e803090f8eebcb960a884d5c6aa1c86414fc
Ahmet Kilinc <akx9@icloud.com>
2022-01-04 15:48:34 +0000

    blur images on load

--- components/PhotographyGrid.tsx
+++ components/PhotographyGrid.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 const PhotographyGrid = () => {
 	return (
 		<div className={styles.photographyWrapper}>
-			{Photography.map(({ img }) => (
-				<Image key={img} src={`/images/photographs/${img}.jpeg`} alt={img} height="320px" width="100%" />
-			))}
+			{Photography.map(({ img }) => {
+				return <Image key={img} src={require(`../public/images/photographs/${img}.jpeg`)} alt={img} height="320px" width="100%" placeholder="blur" />;
+			})}
 		</div>
 	);
 };
