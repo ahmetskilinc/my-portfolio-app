0da548c1fc1aecfbf6f16d5c34430f242edf66e4
Ahmet Kilinc <akx9@icloud.com>
2022-01-06 00:05:55 +0000

    add gtag for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: any) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return <Component {...pageProps} />;
 }
 
