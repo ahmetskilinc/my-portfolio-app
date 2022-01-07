fe4a1bffe89849de7405c1169a23bf00a7e5f8b6
Ahmet Kilinc <akx9@icloud.com>
2022-01-07 21:57:41 +0000

    fix app bar photography

--- components/PhotographyHeader.tsx
+++ components/PhotographyHeader.tsx
@@ -1,8 +1,18 @@
-import styles from "../styles/PhotographyHeader.module.scss";
+import Link from "next/link";
+import { Subheading } from ".";
+import styles from "../styles/BlogAppBar.module.scss";
 
 const PhotographyHeader = () => (
-	<div className={styles.photographyHeaderWrapper}>
-		<h1>My Amateur Photography</h1>
+	<div className={styles.blogAppBarWrapper}>
+		<div className={styles.blogAppBarLeft}>
+			<Link href="/" passHref>
+				<a>Home</a>
+			</Link>
+		</div>
+		<div className={styles.blogAppBarCenter}>
+			<Subheading>My Amatuer Photography</Subheading>
+		</div>
+		<div className={styles.blogAppBarRight}></div>
 	</div>
 );
 
