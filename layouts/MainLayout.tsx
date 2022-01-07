b50f98f4ca4357ead52c4c66ec006c42cf75f39f
Ahmet Kilinc <akx9@icloud.com>
2022-01-07 20:22:05 +0000

    restore from glass

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,11 +1,10 @@
-import styles from "../styles/MainLayout.module.scss";
 import { Footer, Slider } from "../components";
 
 const MainLayout = (props: { children: React.ReactNode }) => {
 	return (
 		<>
 			<Slider />
-			<div className={styles.wrapper}>
+			<div>
 				{props.children}
 				<Footer />
 			</div>
