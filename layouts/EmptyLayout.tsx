f7a46139188489db8f0e899df77af8d29c121dae
Ahmet Kilinc <akx9@icloud.com>
2022-01-16 20:47:36 +0000

    add links to empty layout

--- layouts/EmptyLayout.tsx
+++ layouts/EmptyLayout.tsx
@@ -5,9 +5,15 @@ const EmptyLayout = (props: { children: React.ReactNode }) => {
 	return (
 		<>
 			<SectionWrapper>{props.children}</SectionWrapper>
-			<div style={{ textAlign: "center" }}>
+			<div style={{ textAlign: "center", display: "grid", gridGap: "12px", flex: "row" }}>
 				<Link href="/" passHref>
 					<a>/home</a>
+				</Link>
+				<Link href="/photography" passHref>
+					<a>/photography</a>
+				</Link>
+				<Link href="/tech-stack" passHref>
+					<a>/tech-stack</a>
 				</Link>
 			</div>
 		</>
