74a76ddf8ac7359eb5f19a5851bfad87f28b58f2
Ahmet Kilinc <akx9@icloud.com>
2022-01-17 16:12:07 +0000

    fix some stuff

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import { Footer, Slider } from "../components";
 
 const MainLayout = (props: { children: React.ReactNode }) => {
 	return (
 		<>
+			<Head>
+				<meta property="og:url" content="https://ahmetk.dev" />
+				<meta property="twitter:url" content="https://ahmetk.dev" />
+			</Head>
 			<Slider />
 			<div>
 				{props.children}
